4e92b4894ba479a676e14bc5d8395968d3469d59
Erik Schaal <schaa077@umn.edu>
2020-07-15 18:33:35 -0500

    tried to format the output

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 const inquirer = require("inquirer");
+//const cTable = require('console.table');
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -19,11 +20,22 @@ connection.connect(async function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
     try{
-        var choices = ["Quit"];
+        var choices = ["View departments", "View Roles", "View Employees","Quit"];
         do{
             var selection = await inquirer.prompt(
                 {type:"list",message:"What would you like to do?", choices:choices, name:"option"}
             );
+              //console.log("\n\n\n");
+            switch(selection.option){
+              case "View departments":
+                await viewContent("department");
+                break;
+              case "View Roles":
+                await viewContent("role");
+                break;
+              case "View Employees":
+                await viewContent("employee");
+            }
 
         }while(selection.option !== "Quit")
 
@@ -31,9 +43,20 @@ connection.connect(async function(err) {
         console.log(err);
     }
 
-    afterConnection();
+    //afterConnection();
+    connection.end();
 });
 
+async function viewContent(type) {
+  connection.query(`SELECT * FROM ${type}`,  function(err, res) {
+    if (err) throw err;
+    //console.log("function triggers.")
+    console.log("\n")
+    console.table(res);
+    console.log("\n\n\n");
+    
+  });
+}
 function afterConnection() {
     connection.query("SELECT * FROM department", function(err, res) {
       if (err) throw err;
